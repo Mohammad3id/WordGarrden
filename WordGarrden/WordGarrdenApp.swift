8e7bb1ac26bfdf2d27aefdca726fdfe3475efd62
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2024-10-29 04:30:34 +0300

    Initial Commit

---
+++ WordGarrden/WordGarrdenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarrdenApp.swift
+//  WordGarrden
+//
+//  Created by Mohammad Eid on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordGarrdenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
